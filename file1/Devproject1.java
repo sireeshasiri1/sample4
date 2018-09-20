a92051d15bede499c9599d30df70e9d8c86cd545
sireeshasiri1 <psireeshareddy35@gmail.com>
2018-09-20 23:19:12 +0530

    first commit

---
+++ file1/Devproject1.java
@@ -0,0 +1,8 @@
+public class Devproject1{
+      public static void main(String[] args){
+             System.out.println("hi");
+             System.out.println("hello");
+             System.out.println("welcome");
+             System.out.println("good morning");
+         }
+    }
